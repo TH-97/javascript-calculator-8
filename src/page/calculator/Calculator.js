35ac877130376c7d7df68c73971f1f56f83636ad
TH_97 <hood3392@gmail.com>
2025-10-20 22:48:33 +0900

    feat(calculator) : 추가된 기능에 의해 호출 기능 추가

--- src/page/calculator/Calculator.js
+++ src/page/calculator/Calculator.js
@@ -1,5 +1,8 @@
+import { parseCustomDelimiter } from "../../features/checkDelimiter/index.js";
 import { readInput } from "../../features/input/index.js";
 import { promptOutput, resultOutput } from "../../features/output/index.js";
+import { splitInput } from "../../features/splitInput/index.js";
+import { sumNumber } from "../../features/sumNumber/index.js";
 import { vaildateInput } from "../../features/validate/index.js";
 export async function calculator() {
   await promptOutput();
@@ -8,4 +11,14 @@ export async function calculator() {
     resultOutput(0);
     return;
   }
+  const parsedInput = parseCustomDelimiter(input);
+
+  const numbers = await splitInput(
+    parsedInput.Input,
+    parsedInput.DelimiterArray
+  );
+
+  const result = await sumNumber(numbers);
+  resultOutput(result);
+  return;
 }
