5d6d41dd448280943216865171a041ad31c324f9
TH_97 <hood3392@gmail.com>
2025-10-20 22:48:03 +0900

    fix(parseCustomDelimiter) : 파일명 변경 및 기능 변경

---
+++ src/features/checkDelimiter/parseCustomDelimiter/parseCustomDelimiter.js
@@ -0,0 +1,28 @@
+import { Delimiters } from "../../../entities/delimiter/index.js";
+import { customError } from "../../../shared/error/index.js";
+
+export function parseCustomDelimiter(input) {
+  const startMarker = input.indexOf("//");
+  const endMarker = input.indexOf("\\n");
+
+  const delimiterManager = new Delimiters();
+  let inputString = input;
+
+  if (
+    (startMarker === -1 && endMarker !== -1) ||
+    (startMarker === 0 && endMarker === -1)
+  ) {
+    customError();
+  }
+
+  if (startMarker === 0 && endMarker !== -1) {
+    const customDelimiter = input.slice(startMarker + 2, endMarker);
+    delimiterManager.addCustumDelimiter(customDelimiter);
+    inputString = input.slice(endMarker + 2);
+  }
+
+  return {
+    DelimiterArray: delimiterManager.getDelimiterArray(),
+    Input: inputString,
+  };
+}
